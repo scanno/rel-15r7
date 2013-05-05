e0c78b538c7d406805e7c1957c7357fe40e236e9
Clemente <clemsyn@yahoo.com>
2013-05-04 21:08:25 -0500

    Update tegra2_dvfs.c
    fix SOD on some tegra's that is unable to handle a high nominal voltage

--- arch/arm/mach-tegra/tegra2_dvfs.c
+++ arch/arm/mach-tegra/tegra2_dvfs.c
@@ -64,11 +64,11 @@ static const int cpu_millivolts[MAX_DVFS_FREQS] =
 
 static const int cpu_speedo_nominal_millivolts[] =
 /* spedo_id  0,    1,    2 */
-	{ 1400, 1400, 1400 };
+	{ 1400, 1250, 1400 };
 
 static const int core_speedo_nominal_millivolts[] =
 /* spedo_id  0,    1,    2 */
-	{ 1450, 1450, 1450 };
+	{ 1450, 1300, 1450 };
 	
 #endif
 
@@ -86,7 +86,7 @@ static struct dvfs_rail tegra2_dvfs_rail_vdd_cpu = {
 #ifndef CONFIG_TEGRA_ENABLE_OC
 	.nominal_millivolts = 1125,
 #else
-	.nominal_millivolts = 1400,
+	.nominal_millivolts = 1250,
 #endif
 };
 
@@ -101,7 +101,7 @@ static struct dvfs_rail tegra2_dvfs_rail_vdd_core = {
 #ifndef CONFIG_TEGRA_ENABLE_OC
 	.nominal_millivolts = 1225,
 #else
-	.nominal_millivolts = 1450,
+	.nominal_millivolts = 1300,
 #endif
 	.step = 150, /* step vdd_core by 150 mV to allow vdd_aon to follow */
 };
@@ -117,7 +117,7 @@ static struct dvfs_rail tegra2_dvfs_rail_vdd_aon = {
 #ifndef CONFIG_TEGRA_ENABLE_OC
 	.nominal_millivolts = 1225,
 #else
-	.nominal_millivolts = 1450,
+	.nominal_millivolts = 1300,
 #endif
 #ifndef CONFIG_TEGRA_CORE_DVFS
 	.disabled = true,
